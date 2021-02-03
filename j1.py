09bd1846dc4a7110e44e0860ec5658e12bc014dd
vraj137 <69980892+vraj137@users.noreply.github.com>
2021-02-03 12:04:15 -0500

    Add files via upload

---
+++ j1.py
@@ -0,0 +1,10 @@
+small = int(input());
+medium = int(input());
+large = int(input());
+
+total = (1*small) + (2*medium) + (3*large);
+
+if total >= 10:
+    print("happy");
+else:
+    print("sad");
